4e7d1ece1cfd7bb5818822d6fb95e3d9c3433553
Isadora XPS <isadoravuongvan@gmail.com>
2024-05-26 13:31:39 +0200

    responsive nav - suspense added to form signin

--- app/_components/auth/form-signin.tsx
+++ app/_components/auth/form-signin.tsx
@@ -5,6 +5,7 @@ import { FiAlertCircle, FiEye, FiEyeOff, FiKey, FiMail, FiUser, FiArrowRight } f
 import { useFormState, useFormStatus } from 'react-dom';
 import { authenticate } from '@/app/lib/actions_auth';
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
 import SubmitButton from "../submit-button";
 
@@ -27,78 +28,77 @@ export default function FormSignIn() {
     const justSignedUp = searchParams.get('signedup')
     return (
         // <form action={handleSignIn}>
-        <form action={dispatch}>
-        <div
-          className="flex h-8 items-end space-x-1"
-          aria-live="polite"
-          aria-atomic="true"
-        >
-          {justSignedUp && (
-            <>
-              <FiAlertCircle className="h-5 w-5 text-info-content" />
-              <p className="text-sm text-info-content">Thank you for signing up. You can now sign in.</p>
-            </>
-          )}
-        </div>
-
-             <label className="input input-bordered flex items-center gap-2">
-                {/* <FiMail className="w-4 h-4 opacity-70" /> */}
-                <input 
-                    type="email" 
-                    id="email" 
-                    name="email"
-                    className="grow" 
-                    placeholder="Email"
-                    defaultValue={justSignedUp || ""}
-                />
-             </label>
-             {/* <label className="input input-bordered flex items-center gap-2">
-                 <FiUser className="w-4 h-4 opacity-70" />
-                 <input type="text" className="grow" placeholder="Name" />
-             </label> */}
-             <label className="input input-bordered flex items-center gap-2">
-                {/* <FiKey className="w-4 h-4 opacity-70" /> */}
-                <input
-                    id="password"
-                    name="password"
-                    type={passwordVisible ? 'text' : 'password'}
-                    className="grow"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    onClick={() => {
-                        if (!hasTypedPassword) {
-                            setPassword('');
-                            setHasTypedPassword(true);
-                        }
-                    }}
-                    onInput={() => setHasTypedPassword(true)}
-                />
-                <span
-                    className=""
-                    onClick={() => setPasswordVisible((prevVisible) => !prevVisible)}
-                >
-                    {passwordVisible ? <FiEyeOff className="w-4 h-4 opacity-70" /> : <FiEye className="w-4 h-4 opacity-70" />}
-                </span>
-            </label> 
-            
-            {/* <button className="btn btn-secondary">Sign In</button> */}
-            <LoginButton />
-            {/* <SubmitButton pending={useFormStatus()} submitMessage="Sign In" submittingMessage="Signing in..." color="primary" /> */}
-
+        <Suspense>
+            <form action={dispatch}>
             <div
-            className="flex h-8 items-end space-x-1"
-            aria-live="polite"
-            aria-atomic="true"
+              className="flex h-8 items-end space-x-1"
+              aria-live="polite"
+              aria-atomic="true"
             >
-            {errorMessage && (
+              {justSignedUp && (
                 <>
-                <FiAlertCircle className="h-5 w-5 text-error" />
-                <p className="text-sm text-error">{errorMessage}</p>
+                  <FiAlertCircle className="h-5 w-5 text-info-content" />
+                  <p className="text-sm text-info-content">Thank you for signing up. You can now sign in.</p>
                 </>
-            )}
+              )}
             </div>
-
-        </form>
+                 <label className="input input-bordered flex items-center gap-2">
+                    {/* <FiMail className="w-4 h-4 opacity-70" /> */}
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="grow"
+                        placeholder="Email"
+                        defaultValue={justSignedUp || ""}
+                    />
+                 </label>
+                 {/* <label className="input input-bordered flex items-center gap-2">
+                     <FiUser className="w-4 h-4 opacity-70" />
+                     <input type="text" className="grow" placeholder="Name" />
+                 </label> */}
+                 <label className="input input-bordered flex items-center gap-2">
+                    {/* <FiKey className="w-4 h-4 opacity-70" /> */}
+                    <input
+                        id="password"
+                        name="password"
+                        type={passwordVisible ? 'text' : 'password'}
+                        className="grow"
+                        value={password}
+                        onChange={handlePasswordChange}
+                        onClick={() => {
+                            if (!hasTypedPassword) {
+                                setPassword('');
+                                setHasTypedPassword(true);
+                            }
+                        }}
+                        onInput={() => setHasTypedPassword(true)}
+                    />
+                    <span
+                        className=""
+                        onClick={() => setPasswordVisible((prevVisible) => !prevVisible)}
+                    >
+                        {passwordVisible ? <FiEyeOff className="w-4 h-4 opacity-70" /> : <FiEye className="w-4 h-4 opacity-70" />}
+                    </span>
+                </label>
+            
+                {/* <button className="btn btn-secondary">Sign In</button> */}
+                <LoginButton />
+                {/* <SubmitButton pending={useFormStatus()} submitMessage="Sign In" submittingMessage="Signing in..." color="primary" /> */}
+                <div
+                className="flex h-8 items-end space-x-1"
+                aria-live="polite"
+                aria-atomic="true"
+                >
+                {errorMessage && (
+                    <>
+                    <FiAlertCircle className="h-5 w-5 text-error" />
+                    <p className="text-sm text-error">{errorMessage}</p>
+                    </>
+                )}
+                </div>
+            </form>
+        </Suspense>
     )
 }
 
