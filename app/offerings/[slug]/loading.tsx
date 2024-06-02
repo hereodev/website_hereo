287094ff105341b729458c7e58b8ca721028684a
Isadora XPS <isadoravuongvan@gmail.com>
2024-06-02 12:42:29 +0200

    bash commit

---
+++ app/offerings/[slug]/loading.tsx
@@ -0,0 +1,10 @@
+
+
+export default function OfferingLoading() {
+
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <span className="loading loading-ring loading-lg"></span>
+        </div>
+    )
+}
