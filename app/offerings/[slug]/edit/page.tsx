2626defea6e2bfa6907bf6db7e14451ea5ec6ffb
Isadora XPS <isadoravuongvan@gmail.com>
2024-07-04 02:27:16 +0200

    bash commit

--- app/offerings/[slug]/edit/page.tsx
+++ app/offerings/[slug]/edit/page.tsx
@@ -1,14 +1,32 @@
 import UploadForm from "@/app/_components/upload/upload-form";
 import { auth } from "@/auth"
-
+import prisma from "@/prisma";
 
 export default async function EditOffering({ params }: { params: { slug: string } }) {
     const session = await auth();
+
+    const offering = await prisma.art.findUnique({
+        where: {
+            slug: params.slug
+        },
+        include: {
+            associated_media: {
+                include: {
+                    Media: true
+                }
+            },
+            uploader: true,
+            authors: true,
+        }
+    })
     return (
         <main>
             {
                 session && session.user && session.user.id &&
-                    <div>Editing: {params.slug}</div>
+                    <div>
+                        <p>Editing: {params.slug}</p>
+                        <pre>{JSON.stringify(offering, null, 2)}</pre>
+                    </div>
             }
         </main>
     )
