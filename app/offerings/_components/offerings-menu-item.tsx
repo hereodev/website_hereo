b0b74a13f1497de00ac85e1af0fadf4df330294c
Isadora XPS <isadoravuongvan@gmail.com>
2024-08-09 23:06:09 +0200

    bash commit

--- app/offerings/_components/offerings-menu-item.tsx
+++ app/offerings/_components/offerings-menu-item.tsx
@@ -41,8 +41,8 @@ export default function OfferingsMenuItem({
     return (
         <div>
             {/* <p className="text-xs">{chosenCategories.join(', ')}</p> */}
-            <div className="flex flex-row justify-between group hover:cursor-pointer" onClick={() => setIsExtended(!isExtended)}>
-                <p className="border-t border-t-white group-hover:font-semibold group-hover:text-primary group-hover:border-t-primary">{label.toUpperCase()}</p>
+            <div className="flex flex-row justify-between group hover:cursor-pointer hover:text-primary" onClick={() => setIsExtended(!isExtended)}>
+                <p className="border-t border-t-white group-hover:font-semibold group-hover:border-t-primary">{label.toUpperCase()}</p>
                 <p>
                     {count}
                     {
