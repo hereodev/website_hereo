6acb9fab51fb7bc88e659b27a236a8018dec5e36
Isadora XPS <isadoravuongvan@gmail.com>
2024-08-13 19:50:24 +0200

    bash commit

--- app/offerings/upload/loading.tsx
+++ app/offerings/upload/loading.tsx
@@ -1,13 +1,84 @@
 
 
-export default function OfferingUploadLoading() {
+// export default function OfferingUploadLoading() {
 
-    return (
-        <div>
-            <h1>Upload Work</h1>    
-        <div className="w-full h-screen flex justify-center items-center">
-            <span className="loading loading-ring loading-lg"></span>
+//     return (
+//         <div>
+//             <h1>Upload Work</h1>    
+//             <div className="w-full h-screen flex justify-center items-center">
+//                 <span className="loading loading-ring loading-lg"></span>
+//             </div>
+//         </div>
+//     )
+// }
+
+
+// loading.tsx
+
+import React from 'react';
+
+const LoadingSkeleton: React.FC = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <h1>Upload Work</h1>
+      <div className="form-control w-full">
+        {/* Title */}
+        <div className="label pb-1">
+          <span className="label-text title-txt">Title</span>
         </div>
+        <div className="skeleton h-12 w-full"></div>
+      </div>
+
+      <div className="form-control w-full">
+        {/* Subtitle */}
+        <div className="label pb-1">
+          <span className="label-text title-txt">Subtitle</span>
         </div>
-    )
-}
+        <div className="skeleton h-12 w-full"></div>
+      </div>
+
+      <div className="form-control w-full">
+        {/* Categories */}
+        <div className="label pb-1">
+          <span className="label-text title-txt">Prompt(s)</span>
+        </div>
+        <div className="skeleton h-12 w-full"></div>
+      </div>
+
+      <div className="form-control w-full">
+        {/* Authors */}
+        <div className="label pb-1">
+          <span className="label-text title-txt">Author(s)</span>
+        </div>
+        <div className="skeleton h-8 w-full"></div>
+        <div className="skeleton h-8 w-full mt-2"></div>
+      </div>
+
+      <div className="form-control w-full">
+        {/* Content (Tiptap) */}
+        <div className="label pb-1">
+          <span className="label-text title-txt">Content</span>
+        </div>
+        <div className="skeleton h-32 w-full"></div>
+      </div>
+
+      <div className="form-control w-full">
+        {/* Media */}
+        <div className="label pb-1">
+          <span className="label-text title-txt">Media</span>
+        </div>
+        <div className="skeleton h-12 w-full"></div>
+      </div>
+
+      <div className="form-control w-full">
+        {/* Link */}
+        <div className="label pb-1">
+          <span className="label-text title-txt">Link</span>
+        </div>
+        <div className="skeleton h-12 w-full"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSkeleton;
