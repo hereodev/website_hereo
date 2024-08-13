14694a088ccf48052ac1984e4815074ddfdbeb6c
Isadora XPS <isadoravuongvan@gmail.com>
2024-08-13 19:40:37 +0200

    bash commit

---
+++ app/offerings/upload/loading.tsx
@@ -0,0 +1,13 @@
+
+
+export default function OfferingUploadLoading() {
+
+    return (
+        <div>
+            <h1>Upload Work</h1>    
+        <div className="w-full h-screen flex justify-center items-center">
+            <span className="loading loading-ring loading-lg"></span>
+        </div>
+        </div>
+    )
+}
